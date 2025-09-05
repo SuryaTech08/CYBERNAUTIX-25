ce5177ced97bceb6e0faa323f442a808a5f42786
Shashwanth N <155833351+ShashwanthN@users.noreply.github.com>
2025-09-05 15:58:25 +0530

    Update firebase.js
    dont push api keys

--- src/backend/firebase.js
+++ src/backend/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCW5gEtw8m7g-9dXsZ2vQbzQXX4T--zm_s",
-    authDomain: "cybernautix-25.firebaseapp.com",
-    projectId: "cybernautix-25",
-    storageBucket: "cybernautix-25.firebasestorage.app",
-    messagingSenderId: "239279195224",
-    appId: "1:239279195224:web:45532ef380ca72386f762d"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 // Initialize Firebase
